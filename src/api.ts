0b25e2681493545486492ba8ae0a67118d37419a
Votre Nom <aristidegaelkouandjakenfack@gmail.com>
2026-02-13 04:07:48 +0100

    ajout de l'environnement

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+const rawApiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = rawApiBaseUrl.replace(/\/+$/, '');
 const AUTH_TOKEN_STORAGE_KEY = 'afpd_auth_token';
 
 interface FetchOptions extends RequestInit {
